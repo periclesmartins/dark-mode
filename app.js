373ebfaa19b36792a7a2536a6d9a7ac9cff8b94a
periclesperes <periclesperes01@gmail.com>
2022-10-11 14:26:54 -0300

    firts commit

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict'
+const btn = document.querySelector('.btn')
+const container = document.querySelector('.container')
+
+btn.onclick = function()
+{
+    this.classList.toggle('active')
+    container.classList.toggle('active')
+}
